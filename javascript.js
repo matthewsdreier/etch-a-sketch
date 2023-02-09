fca144d016f265e6b1570252dbf7e8b6e01ddd76
Matthew Dreier <43585853+matthewsdreier@users.noreply.github.com>
2023-02-09 12:58:25 -0500

    First commit. Create files, add createBoard function.
    Create files index.html, javascript.js, style.css.
    
    Add function createBoard to javascript.js. This function creates a
    n x n board of divs that will be used as the drawing board.

---
+++ javascript.js
@@ -0,0 +1,17 @@
+let createBoard = function(sideLength, board) {
+    for (let i = 0; i < sideLength; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        board.appendChild(row);
+        for (let j = 0; j < sideLength; j++) {
+            const square = document.createElement("div")
+            row.appendChild(square);
+        }
+
+    }
+}
+
+board = document.querySelector(".board");
+sideLength = 4;
+
+createBoard(sideLength, board);
